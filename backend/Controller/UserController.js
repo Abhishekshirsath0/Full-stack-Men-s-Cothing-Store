76f54b41f1a948d496f13b924bed0cfe2a19a557
Abhishek Shirsath <149806302+Abhishekshirsath0@users.noreply.github.com>
2026-05-08 00:34:48 +0530

    Update UserController.js

--- backend/Controller/UserController.js
+++ backend/Controller/UserController.js
@@ -42,8 +42,8 @@ export const postuserData = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite:none,
       maxAge: 2 * 24 * 60 * 60 * 1000,
     });
 
